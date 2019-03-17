509eb9a7bb8e2fb4e7c5756326f1f26fff9b20f9
kam <kmathes348@students.mchenry.edu>
2019-03-17 17:26:27 -0500

    Initial commit

---
+++ app/src/main/java/mad155/kmathes/mad155module10northernlightsanimation/Tween.java
@@ -0,0 +1,21 @@
+package mad155.kmathes.mad155module10northernlightsanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Tween extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tween);
+
+        ImageView imgRotate = (ImageView) findViewById(R.id.imgTween);
+
+        imgRotate.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotation));
+
+    }
+}
